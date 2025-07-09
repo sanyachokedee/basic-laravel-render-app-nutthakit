85d5ecd84b44f63314f640591df21eb1966bdab1
sanyachokedee <ing11819@gmail.com>
2025-07-09 11:04:53 +0700

    3

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,27 @@
-import react from '@vitejs/plugin-react'
-import laravel from 'laravel-vite-plugin'
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            refresh: true,
-            publicDirectory: 'public',            
-        }),
-        react(),
-        tailwindcss(),
-    ],
-    server: {
-        https: false, //เปิดการใช้งาน HTTPS
-    },
-    // base: '/laventoryapp/', //กำหนด base path ของแอปพลิเคชัน
-    // base: '/pedantic-booth.203-170-190-139.plesk.page/', //กำหนด base path ของแอปพลิเคชัน
-    base: '/build/', //กำหนด base path ของแอปพลิเคชัน
-    build: {
-        outDir: 'public/build', //กำหนด output directory สำหรับการ build
-        emptyOutDir: true, //ล้าง output directory ก่อนการ build ใหม่
-        assetsDir:'' // ทำให้ไฟล์ css และ js อยู่ใน root ของ public/build
-    },    
-    esbuild: {
-        jsx: 'automatic',
-    },
-})
+export default defineConfig(() => {
+
+    return {
+        plugins: [
+            laravel({
+                input: ["resources/css/app.css", "resources/js/app.js"],
+                refresh: false,
+            }),
+            tailwindcss(),
+        ],
+        server: {
+            https: true, // Enable HTTPS
+        },
+        base: "/build/", // หรือปล่อยว่างเป็น '' เพื่อให้ Vite ใช้ relative path
+        build: {
+            outDir: "public/build",
+            assetsDir: "", // ทำให้ assets ไม่อยู่ในโฟลเดอร์ย่อย
+        },
+        esbuild: {
+            jsx: "automatic",
+        },
+    };
+});
